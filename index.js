4c9903a059543bb13ec1cb9fd8a77a2ba2b4d0b4
Ainzar-00 <younessainzar2@gmail.com>
2026-04-02 11:54:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/drive"
 ];
 
-const CENTRAL_SHEET_ID = "1Wj0LzFPa5esKLCjGW_UfsdYUAtOIJ-YnhKiCA8X9o44";
+const CENTRAL_SHEET_ID = "1L-ok2OzJhC5JnRbzKoCsieFK88u5PJyv7uMospBwIGs";
 const APPS_SCRIPT_URL  = "https://script.google.com/macros/s/AKfycbxg27IYiHhP9MXxbvFgzjwM72PIZb7yPbafA9gHTnmCwPCHdlR1gHPQaBs4nbUxazM7/exec";
 const REDIRECT_URI     = process.env.REDIRECT_URI || "http://localhost:8080/oauth2callback";
 
